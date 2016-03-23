ff4f8a7bfc0cbe07e1da65a8d3941f1897f26f32
Jeannine Hamilton <jeannine.hamilton@gmail.com>
2016-03-23 14:25:45 -0500

    finished user story 3 except tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,13 +2,21 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'bundler/setup'
 Bundler.setup
 require 'kele'
+require 'rspec'
 require 'webmock/rspec'
 require 'json'
-require "test/spec/lib"
 
-  RSpec.configure do |config|
-    config.before(:each) do
-      allow(Kele).to receive(:any).and_return('stubbed')
-    end
-  end
-end
+WebMock.disable_net_connect!(allow_localhost: true)
+
+  # RSpec.configure do |config|
+  #   config.before :each do
+  #     stub_request(:post, "email:password@",api_url("sessions")).
+  #       with(headers: {}).
+  #       to_return(status: 200, body: "", headers: {})
+  #     end
+  #   end
+  #
+  # private
+  # def api_url(endpoint)
+  #   "https://private-4967b7-blocapi.apiary-mock.com/api/v1/#{endpoint}"
+  # end
