0181a8329dd657bd7248928fa9a53bfcb6817444
Jeannine Hamilton <jeannine.hamilton@gmail.com>
2016-03-23 20:23:04 -0500

    finished everything except tests

--- lib/kele.rb
+++ lib/kele.rb
@@ -16,6 +16,11 @@ class Kele
     response = self.class.get(api_url("users/me"), headers: { "authorization" => @auth_token })
     body = JSON.parse(response.body)
   end
+  #when I run this below I get message: resource not found; it is this way on api console as well
+  def get_mentor_availability(mentor_id)
+    response = self.class.get(api_url("mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
+    body = JSON.parse(response.body)
+  end
 
   private
   def api_url(endpoint)
